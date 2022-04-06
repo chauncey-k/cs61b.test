ad612ab0e03fefd610603a7d91ed9333bfac3847
Chauncey <chaunceyk213@gmail.com>
2022-04-06 11:14:59 +0800

    try

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -2,13 +2,13 @@ public class ArrayDeque<T> {
     private int size;
     private T[] array;
 
-    private void resize(int newSize) {
+    public void resize(int newSize) {
         T[] resizedArray = (T[]) new Object[newSize];
         System.arraycopy(array, 0, resizedArray, 0, size);
         array = resizedArray;
     }
 
-    private void testUsage() {
+    public void testUsage() {
         if (size / array.length <= 0.25) {
             size /= 2;
         }
