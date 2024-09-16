5cb4c6724980df701fbc1f0075a106caf46e8b96
chauncey-k <1073695360@qq.com>
2024-09-16 11:05:02 +0800

    modify proj1a ArrayDeque totally

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,46 +1,58 @@
 public class ArrayDeque<T> {
+    // the number of elements stored in array
     private int size;
+
+    // former index of the first element
+    private int nextFirst;
+
+    // store the address of the array
     private T[] array;
 
+    // next index of the last element
+    private int nextLast;
+
     private void resize(int newSize) {
-        if (newSize < 8) {
-            T[] resizedArray = (T[]) new Object[8];
-        }
         T[] resizedArray = (T[]) new Object[newSize];
-        System.arraycopy(array, 0, resizedArray, 0, size);
+        for (int i = nextFirst + 1; i < size; i += 1) {
+            resizedArray[newSize - 1 - (size - 1 - i)] = array[i];
+        }
+        for (int j = 0; j < nextLast; j += 1) {
+            resizedArray[j] = array[j];
+        }
+        nextFirst = newSize - 1 - (size - 1 - nextFirst);
         array = resizedArray;
     }
 
     private void testUsage() {
-        if (size / array.length <= 0.25) {
-            size /= 2;
+        if (array.length >= 16) {
+            if (size * 1.0 / array.length < 0.25){
+                resize(size / 4);
+            }
         }
     }
 
     public void addFirst(T item) {
-        int i = 0;
-        if (size == array.length) {
+        if (nextFirst + 1 == nextLast) {
             resize(size * 2);
         }
-        if (size != 0) {
-            i = size - 1;
-            while (i >= 0) {
-                array[i + 1] = array[i];
-                --i;
-            }
+        array[nextFirst] = item;
+        nextFirst -= 1;
+        if (nextFirst < 0) {
+            nextFirst += array.length;
         }
-        array[0] = item;
-        ++size;
+        size += 1;
 
         testUsage();
     }
 
     public void addLast(T item) {
-        if (size == array.length) {
+        if (nextFirst + 1 == nextLast) {
             resize(size * 2);
         }
-        array[size] = item;
-        ++size;
+        array[nextLast] = item;
+        nextLast += 1;
+        nextLast = nextLast % array.length;
+        size += 1;
 
         testUsage();
     }
@@ -60,7 +72,7 @@ public class ArrayDeque<T> {
                 System.out.println(array[i]);
             }
             System.out.print(array[i] + " ");
-            ++i;
+            i += 1;
         }
     }
 
@@ -72,10 +84,10 @@ public class ArrayDeque<T> {
         int i = 1;
         while (i < size) {
             array[i - 1] = array[i];
-            ++i;
+            i += 1;
         }
         array[size] = null;
-        --size;
+        size -= 1;
 
         testUsage();
         return temp;
@@ -87,22 +99,24 @@ public class ArrayDeque<T> {
         }
         T temp = array[size - 1];
         array[size - 1] = null;
-        --size;
+        size -= 1;
 
         testUsage();
         return temp;
     }
 
     public T get(int index) {
-        if (index >= size) {
+        if (index >= size || index < 0) {
             return null;
         }
-        return array[index];
+        return array[(nextFirst + 1 + index) % array.length];
     }
 
     public ArrayDeque() {
         size = 0;
         array = (T[]) new Object[8];
+        nextFirst = 0;
+        nextLast = 7;
     }
 
 }
